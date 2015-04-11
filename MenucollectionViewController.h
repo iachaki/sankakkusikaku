eb1670ec94deeeee11172401091c94502b89fa24
Ia Kawasaki <ia_0212justin-716cherry@softbank.ne.jp>
2015-04-11 23:49:01 +0900

    add new file、ゲームのレベルを選択する画面をCollectionViewでつくる

---
+++ MenucollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenucollectionViewController.h
+//  sankakkusikaku
+//
+//  Created by ia on 2015/04/11.
+//  Copyright (c) 2015年 Life is tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenucollectionViewController : UIViewController
+
+@end
